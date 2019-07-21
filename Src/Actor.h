114863fa93155dd0b6d6f2234118e277647a6355
Karolina Jasickova <jasickovak@gmail.com>
2019-07-21 16:55:12 +0200

    Base for character movement

---
+++ Src/Actor.h
@@ -0,0 +1,66 @@
+#ifndef INC_2D_RPG_ACTOR_H
+#define INC_2D_RPG_ACTOR_H
+
+#include <iostream>
+#include <ostream>
+
+struct Vector2D
+{
+    double x;
+    double y;
+
+    Vector2D(double posX, double posY) : x(posX), y(posY) {};
+
+    friend std::ostream& operator<<(std::ostream& os, const Vector2D& vector)
+    {
+        os << "X = " << vector.x << ", Y = " << vector.y << std::endl;
+        return os;
+    }
+};
+
+
+
+enum class Direction
+{
+    NONE,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
+    LEFT_UP,
+    LEFT_DOWN,
+    RIGHT_UP,
+    RIGHT_DOWN,
+
+    SIZE
+};
+
+
+class Actor
+{
+private:
+    unsigned m_id;
+
+protected:
+    Vector2D m_position;
+    double m_speed;
+
+public:
+    Actor() : m_id(0), m_position(0.0, 0.0), m_speed(1.0) {};
+    ~Actor() = default;
+
+    Vector2D GetPosition()
+    {
+        return m_position;
+    }
+
+    double GetSpeed()
+    {
+        return m_speed;
+    }
+
+    void Move(Direction direction);
+};
+
+
+#endif //INC_2D_RPG_ACTOR_H
